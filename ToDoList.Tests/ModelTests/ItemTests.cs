78a8163267b651181f90086fe9a5064c8f096fb3
chynnalew <chynnalew@yahoo.com>
2021-09-27 14:11:24 -0700

    create project following week 8 lessons
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests:IDisposable
+  {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+    
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+    [TestMethod]
+    public void GetDesscription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string updatedDescription = "do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item>{};
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+  }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace Program
+{
+  public class YourClass
+  {
+    static void Main()
+    {
+    
+    }
+  }
+}
